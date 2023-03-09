b4498a961240bafba671338a14ec3933a3d28aaf
VinnyDeAlmeida <vdd28@scarletmail.rutgers.edu>
2023-03-08 21:34:38 -0500

    Added Deck Script

--- Assets/Prospector/__Scripts/JsonParseDeck.cs
+++ Assets/Prospector/__Scripts/JsonParseDeck.cs
@@ -6,39 +6,67 @@ using UnityEngine;
 [System.Serializable]
 public class JsonPip
 {
-    public string type = "pip"; // "pip", "letter", or "suit"
-    public Vector3 loc; // Location of the Sprite on the Card
-    public bool flip = false; // True to flip the Sprite vertically
-    public float scale = 1; // the scale of the Sprite
+	public string type = "pip"; // "pip", "letter", or "suit"
+	public Vector3 loc; // Location of the Sprite on the Card
+	public bool flip = false; // True to flip the Sprite vertically
+	public float scale = 1; // the scale of the Sprite
 }
 
 // This class stores information for each rank of card
 [System.Serializable]
 public class JsonCard
 {
-    public int rank; // The rank (1-13) of this card
-    public string face; // Sprite to use for each face card
-    public List<JsonPip> pips = new List<JsonPip>(); // The pips on this card 
+	public int rank; // The rank (1-13) of this card
+	public string face; // Sprite to use for each face card
+	public List<JsonPip> pips = new List<JsonPip>(); // The pips on this card 
 }
 
 // This class contains information about the entire deck
 [System.Serializable]
 public class JsonDeck
 {
-    public List<JsonPip> decorators = new List<JsonPip>();
-    public List<JsonCard> cards = new List<JsonCard>();
+	public List<JsonPip> decorators = new List<JsonPip>();
+	public List<JsonCard> cards = new List<JsonCard>();
 }
 
 public class JsonParseDeck : MonoBehaviour
 {
-    [Header("Inscribed")]
-    public TextAsset jsonDeckFile; // Reference to the JSON_Deck text file
+	private static JsonParseDeck S { get; set; }
 
-    [Header("Dynamic")]
-    public JsonDeck deck;
+	[Header("Inscribed")]
+	public TextAsset jsonDeckFile; // Reference to the JSON_Deck text file
 
-    void Awake()
-    {
-        deck = JsonUtility.FromJson<JsonDeck>(jsonDeckFile.text);
-    }
+	[Header("Dynamic")]
+	public JsonDeck deck;
+
+	void Awake()
+	{
+		if (S != null)
+		{
+			Debug.LogError("JsonParseDeack.S can't be set a 2nd time!");
+			return;
+		}
+		S = this;
+
+		deck = JsonUtility.FromJson<JsonDeck>(jsonDeckFile.text);
+	}
+
+	/// <summary>
+	/// Returns the decorator layout information for all cards.
+	/// </summary>
+
+	static public List<JsonPip> DECORATORS
+	{
+		get { return S.deck.decorators; }
+	}
+
+	static public JsonCard GET_CARD_DEF(int rank)
+	{
+		if ((rank < 1) || (rank > S.deck.cards.Count))
+		{
+			Debug.LogWarning("Illegal rank argument: " + rank);
+			return null;
+		}
+		return S.deck.cards[rank - 1];
+	}
 }
